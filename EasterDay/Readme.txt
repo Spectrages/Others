8186dd21805e81518ec33a2bbe6fa228e12fa51f
Artsiom Malyshev <58001922+Spectrages@users.noreply.github.com>
2021-03-22 17:06:02 +0200

    Add files via upload

---
+++ EasterDay/Readme.txt
@@ -0,0 +1,3 @@
+Здесь представлена моя реализация математического алгоритма, предназначенного для определения дня празднования Пасхи в любом году. Предложен впервые немецким математиком Карлом Гауссом в 1800 году.
+
+Here is my implementation of a mathematical algorithm designed to determine the day of Easter celebration in any year. It was first proposed by the German mathematician Karl Gauss in 1800.
---
+++ EasterDay/main.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+void oldStyle(int f)
+{
+    if(f <= 9)
+    { cout << 22 + f << " march (Orthodox oldstyle)\n"; }
+    else if (f > 9)
+    { cout << f - 9 << " april (Orthodox oldstyle)\n"; }
+}
+
+void newStyle(int f, int year)
+{
+    if(year >= 1900)
+    { cout << ((f - 9) + 13) << " april (Orthodox newstyle)\n"; }
+    else
+    { }
+}
+
+void ortodox()
+{
+    int a, b, c, d, e, f, year, M = 15, N = 6;
+    char ch;
+    do
+    {
+    system("clear");
+    cout << "Enter year: ";
+    cin >> year;
+    a = year % 19;
+    b = year % 4;
+    c = year % 7;
+    d = ((19 * a) + M) % 30;
+    e = ((2 * b) + (4 * c) + (6 * d) + N) % 7;
+    f = d + e;
+    oldStyle(f);
+    newStyle(f, year);
+    cout << "Repeat? (y/n): ";
+    cin >> ch;
+    }
+    while(ch != 'n');
+}
+
+void catholic()
+{
+    int a, b, c, d, e, k, p, q, M, N, year;
+    char ch;
+    do
+    {
+    system("clear");
+    cout << "Enter year: ";
+    cin >> year;
+    a = year % 19;
+    b = year % 4;
+    c = year % 7;
+    k = year / 100;
+    p = (13 + (8 * k)) / 25;
+    q = k / 4;
+    M = (15 - p + k - q) % 30;
+    N = (4 + k - q) % 7;
+    d = ((19 * a) + M) % 30;
+    e = ((2 * b) + (4 * c) + (6 * d) + N) % 7;
+    if(d == 29 && e == 6)
+        { cout << 19 << " april (Catholic)\n"; }
+    else if((d == 28 && e == 6) && ((11 * M + 11) % 30 < 19))
+        { cout << 18 << " april (Catholic)\n"; }
+    else
+    {
+        if((22 + d + e) < 31)
+            { cout << 22 + d + e << " march (Catholic)\n"; }
+        else if((22 + d + e) > 31)
+            { cout << d + e - 9 << " april (Catholic)\n"; }
+    }
+    cout << "Repeat? (y/n): ";
+    cin >> ch;
+    }
+    while(ch != 'n');
+}
+
+int main()
+{
+    char ch, ans;
+    do
+    {
+        cout << "Ortodox or Catholic? (o/c): ";
+        cin >> ch;
+        switch(ch)
+        {
+            case 'o': ortodox(); break;
+            case 'c': catholic(); break;
+            default: break;
+        }
+        cout << "Continue typing? (y/n): ";
+        cin >> ans;
+    }
+    while(ans != 'n');
+    return 0;
+}
